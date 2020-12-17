8c2f3055e8bafd2b1ca5906fea113256ac8400b4
Kota Kokado <kouta-k@mbm.nifty.com>
2020-12-17 17:06:31 +0900

    シェルスクリプトに書き換え2種類を登録する

--- server.sh
+++ server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./rproxy -scheme $SCHEME -rhost $RHOST -addr $ADDR -ori $ORI -dest $DEST
+./rproxy \
+    -scheme $SCHEME \
+    -rhost $RHOST \
+    -addr $ADDR \
+    -ori $ORI -dest $DEST \
+    -ori $ORI2 -dest $DEST2 \
